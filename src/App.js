e724692685fc6718a519030bad91b6672fce1c2d
Ram Mohan <saxenarhul57009@gmail.com>
2025-01-28 15:41:05 +0530

    ram

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <div className='main'>
+      <div>
+      <input type="password" /><button>rm</button>
+      </div>
+     </div>
+    </>
+  );
+}
+
+export default App;
